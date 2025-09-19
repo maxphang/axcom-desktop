6779a15b7dd34df3bbb55d2904b76b1b6b25548f
maxphang <43926952+maxphang@users.noreply.github.com>
2025-09-19 12:41:27 +0800

    Revert "Persist configuration in user data and document builds"

--- app/preload/settingsPreload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    readConfiguration: () => ipcRenderer.invoke('settings:readConfiguration'),
-    saveConfiguration: (url) => ipcRenderer.invoke('settings:saveConfiguration', url),
-    getSystemVersion: () => ipcRenderer.invoke('settings:getSystemVersion')
-});
